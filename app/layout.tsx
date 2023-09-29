b0ede1676f5d84ba4baad315c70b80ef2459faac
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-09-29 21:24:50 +0330

    Padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir='rtl' className='text-[13px] lg:text-[14px]'>
-      <body className={vazir.className.concat(' p-2 ')}>{children}</body>
+      <body className={vazir.className.concat(' lg:p-2 ')}>{children}</body>
     </html>
   )
 }
