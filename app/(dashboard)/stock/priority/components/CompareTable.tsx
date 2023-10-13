9b21cd264fb7930979d4c5e452f2eb7d1b3d9bb5
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-10-14 00:13:47 +0330

    Add Compare Table

--- app/(dashboard)/stock/priority/components/CompareTable.tsx
+++ app/(dashboard)/stock/priority/components/CompareTable.tsx
@@ -5,8 +5,9 @@ import { Table, TableBody, TableCell, TableHead } from '@/app/(dashboard)/compon
 import { TableRow } from '@/app/(dashboard)/components/TableRow/TableRow'
 import { data } from './data'
 import { useQuery } from 'react-query'
-import { saRequest } from '@/app/config/api'
+// import { saRequest } from '@/app/config/api'
 import { useAllSymbols } from '@/app/hooks'
+import moment from 'jalali-moment'
 
 export const CompareTable = () => {
 
@@ -16,29 +17,45 @@ export const CompareTable = () => {
     return <Table >
         <TableHead>
             <TableRow className='text-center'>
+                <TableCell>نام سهم</TableCell>
                 <TableCell>نماد</TableCell>
                 <TableCell>نماد حق تقدم</TableCell>
-                <TableCell>وضعیت</TableCell>
+                {/* <TableCell>وضعیت</TableCell> */}
                 <TableCell>قیمت نماد پایه</TableCell>
                 <TableCell>قیمت حق تقدم</TableCell>
                 <TableCell>سود درصد</TableCell>
+                <TableCell>تاریخ پایان مهلت استفاده</TableCell>
                 <TableCell>مدت زمان باقی مانده تا پایان مرحله</TableCell>
-                <TableCell>کدال</TableCell>
+                {/* <TableCell>کدال</TableCell> */}
                 <TableCell>TSE</TableCell>
             </TableRow>
         </TableHead>
         <TableBody>
-            {data.map(item => <TableRow className='text-center' hoverEffect>
-                <TableCell>{item.symbol}</TableCell>
-                <TableCell>{item.pSymbol}</TableCell>
-                <TableCell>{item.state}</TableCell>
-                <TableCell>{item.symbolPrice.toLocaleString()}</TableCell>
-                <TableCell>{item.pSymbolPrice.toLocaleString()}</TableCell>
-                <TableCell className={item.profitPercent > 0 ? 'text-green-500 font-bold' : ''}>{item.profitPercent}%</TableCell>
-                <TableCell>{item.daysToEnd} روز</TableCell>
-                <TableCell>کدال</TableCell>
-                <TableCell>TSE</TableCell>
-            </TableRow>)}
+            {data.map(item => {
+
+                const symbolPrice = symbols?.find(s => s.name == item.symbol)?.final_price ?? 0
+                const pSymbolPrice = symbols?.find(s => s.name == item.pSymbol)?.final_price ?? 0
+
+                const symbol = symbols?.find(s => s.name == item.symbol)
+
+                const profitPercent = ((Number(symbolPrice) / (Number(pSymbolPrice) + 1000)) - 1) * 100
+
+                console.log(moment(item?.deadline ?? '', 'jYYYY/jMM/jDD'))
+
+                return <TableRow className='text-center' hoverEffect>
+                    <TableCell className='font-bold'>{symbol?.full_name}</TableCell>
+                    <TableCell>{item.symbol}</TableCell>
+                    <TableCell>{item.pSymbol}</TableCell>
+                    {/* <TableCell>{item.state}</TableCell> */}
+                    <TableCell>{Number(symbolPrice)?.toLocaleString()}</TableCell>
+                    <TableCell>{Number(pSymbolPrice)?.toLocaleString()}</TableCell>
+                    <TableCell className={profitPercent > 0 ? 'text-green-500 font-bold' : ''}>{(symbolPrice && pSymbolPrice) ? Number(profitPercent).toFixed(2) : '-'}%</TableCell>
+                    <TableCell>{item?.deadline} </TableCell>
+                    <TableCell>{item?.deadline ? Math.ceil(moment(item?.deadline, 'jYYYY/jMM/jDD').diff(moment(), 'day',true)) : '-'} روز</TableCell>
+                    {/* <TableCell>کدال</TableCell> */}
+                    <TableCell><a className='text-blue-800' href={`http://www.tsetmc.com/instInfo/${symbols?.find(s => s.name == item.symbol)?.instance_code}`} target='_blank'>لینک به TSE</a></TableCell>
+                </TableRow>
+            })}
         </TableBody>
     </Table>
 
--- app/(dashboard)/stock/priority/components/data.ts
+++ app/(dashboard)/stock/priority/components/data.ts
@@ -1,32 +1,126 @@
 import { PriorityStockItem } from "@/app/types";
 
-export const data:PriorityStockItem [] =[
+export const data:Partial<PriorityStockItem> [] =[
     {
-        symbol:'افزا',
-        pSymbol:'رافزاح',
-        pSymbolPrice:12870,
-        symbolPrice:16040,
+        symbol:'کساپا',
+        pSymbol:'کساپاح',
+        pSymbolPrice:0,
+        symbolPrice:0,
         profitPercent:16,
-        daysToEnd:1,
-        state:'حراج'
+        daysToEnd:20,
+        state:'حراج',
+        deadline:'1402/09/05'
     },
     {
-        symbol:'افزا',
-        pSymbol:'رافزاح',
-        pSymbolPrice:12870,
-        symbolPrice:16040,
-        profitPercent:16,
+        symbol:'قیستو',
+        pSymbol:'قیستوح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
         daysToEnd:1,
-        state:'حراج'
+        state:'حراج',
+        deadline:'1402/08/06'
     },
     {
-        symbol:'افزا',
-        pSymbol:'رافزاح',
-        pSymbolPrice:12870,
-        symbolPrice:16040,
-        profitPercent:16,
+        symbol:'کصدف',
+        pSymbol:'کصدفح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
         daysToEnd:1,
-        state:'حراج'
+        state:'حراج',
+        deadline:'1402/07/24'
+    },
+    {
+        symbol:'تماوند',
+        pSymbol:'تماوندح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/08/28'
+
+    },
+    {
+        symbol:'برکت',
+        pSymbol:'برکتح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/08/04'
+    },
+    // {
+    //     symbol:'ولبهمن',
+    //     pSymbol:'ولبهمنح',
+    //     pSymbolPrice:0,
+    //     symbolPrice:0,
+    //     profitPercent:0,
+    //     daysToEnd:1,
+    //     state:'حراج',
+    //     deadline:'1402/05/26'
+    // },
+    {
+        symbol:'وسپه',
+        pSymbol:'وسپهح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/08/20'
+    },
+    {
+        symbol:'رافزا',
+        pSymbol:'رافزاح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/09/12'
+    },
+    {
+        symbol:'بموتو',
+        pSymbol:'بموتوح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/08/12'
+    },
+    {
+        symbol:'کمرجان',
+        pSymbol:'کمرجانح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/08/24'
+    },
+    {
+        symbol:'دیران',
+        pSymbol:'دیرانح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/08/24'
+    },
+    {
+        symbol:'تنوین',
+        pSymbol:'تنوینح',
+        pSymbolPrice:0,
+        symbolPrice:0,
+        profitPercent:0,
+        daysToEnd:1,
+        state:'حراج',
+        deadline:'1402/09/07'
     }
 
 ]
