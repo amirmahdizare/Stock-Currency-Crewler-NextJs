1db90340b9b5a3adf498c7b6ba1c3d8a003d3233
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-10-15 11:15:15 +0330

    Add Select Base Symbol

--- app/(dashboard)/stock/option/ClientPage.tsx
+++ app/(dashboard)/stock/option/ClientPage.tsx
@@ -1,24 +1,46 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 // import { CompareTable } from './components/CompareTable'
 import { useAllCurrencies, useAllSymbols } from '@/app/hooks'
 import { convertToNumber } from '@/app/utils'
+import { SASymbolListItemType } from '@/app/types'
 
 export const ClientPage = () => {
 
+  const [selectedSymbol, setSelectedSymbols] = useState<SASymbolListItemType | undefined>()
 
   const { data: symbols } = useAllSymbols()
 
   return (
     <div className='flex flex-col gap-4 items-stretch'>
-      <span className='font-bold text-lg border-b pb-2'>آربیتراژ بازار اختبار معامله</span>
-
-     {/* {symbols?.filter((item , index , array))=> {
-      a
-     }} <span></span> */}
+      <span className='font-bold text-lg border-b pb-2'>آربیتراژ بازار اختیار معامله</span>
+      <div className='flex flex-row gap-4 w- full overflow-auto whitespace-nowrap'>
 
 
-      {/* <CompareTable data={[]} /> */}
+        {symbols?.filter((item, index, array) => array.findIndex(inner =>
+          inner.full_name.includes(item.name)
+          && inner.full_name.includes('اخت')
+          && inner.market == '-'
+          && inner.industry == '-') != -1).map(s =>
+            <span onClick={() => setSelectedSymbols(s)} className={'border p-2 rounded-lg  cursor-pointer '.concat(selectedSymbol == s ? 'bg-gray-300 ' : 'hover:bg-gray-50')}>{s.name}</span>
+          )}
+      </div>
+
+
+
+      <div className='grid grid-cols-2 gap-4'>
+        <div className='col-span-2 lg:col-span-1 border rounded-lg p-2'>
+          <span className='font-bold  border-b pb-2'>اختیار خرید</span>
+
+
+        </div>
+
+        <div className='col-span-2 lg:col-span-1 border rounded-lg p-2'>
+          <span className='font-bold  border-b pb-2'>اختیار فروش</span>
+
+        </div>
+
+      </div>
 
 
     </div>
--- app/(dashboard)/stock/option/components/CompareTable.tsx
+++ app/(dashboard)/stock/option/components/CompareTable.tsx
@@ -1,78 +1,77 @@
-// 'use client'
+'use client'
 
-// import React from 'react'
-// import { Table, TableBody, TableCell, TableHead } from '@/app/(dashboard)/components'
-// import { TableRow } from '@/app/(dashboard)/components/TableRow/TableRow'
-// import { data } from './data'
-// import { useQuery } from 'react-query'
-// // import { saRequest } from '@/app/config/api'
-// import { useAllSymbols } from '@/app/hooks'
-// import moment from 'jalali-moment'
-// import { IconCheck, IconX } from '@tabler/icons-react'
-// import { SASymbolListItemType } from '@/app/types'
+import React from 'react'
+import { Table, TableBody, TableCell, TableHead } from '@/app/(dashboard)/components'
+import { TableRow } from '@/app/(dashboard)/components/TableRow/TableRow'
+import { data } from './data'
+import { useQuery } from 'react-query'
+// import { saRequest } from '@/app/config/api'
+import { useAllSymbols } from '@/app/hooks'
+import moment from 'jalali-moment'
+import { IconCheck, IconX } from '@tabler/icons-react'
+import { SASymbolListItemType } from '@/app/types'
 
-// export const CompareTable = ({ data }: { data: Array<SASymbolListItemType> }) => {
+export const CompareTable = ({ data , baseSymbol}: { data: Array<SASymbolListItemType> , baseSymbol:SASymbolListItemType}) => {
 
-//     // const { data: symbols } = useAllSymbols()
-
-
-//     return <Table >
-//         <TableHead>
-//             <TableRow className='text-center'>
-//                 <TableCell>نام سهم</TableCell>
-//                 <TableCell>نماد</TableCell>
-//                 <TableCell>نماد حق تقدم</TableCell>
-//                 {/* <TableCell>وضعیت</TableCell> */}
-//                 <TableCell>قیمت نماد پایه</TableCell>
-//                 <TableCell>قیمت حق تقدم</TableCell>
-//                 <TableCell>سود درصد</TableCell>
-//                 <TableCell>تاریخ پایان مهلت استفاده</TableCell>
-//                 <TableCell>مدت زمان باقی مانده تا پایان مرحله</TableCell>
-//                 <TableCell>برتر به سود بانکی 22%</TableCell>
-//                 <TableCell>برتر به بالاترین سود صندوق درامد ثابت 24%</TableCell>
-//             </TableRow>
-//         </TableHead>
-//         <TableBody>
-//             {data
-//                 .sort((a, b) => {
-//                     const aSymbolPrice = symbols?.find(s => s.name == a.symbol)?.final_price ?? 0
-//                     const aPSymbolPrice = symbols?.find(s => s.name == a.pSymbol)?.final_price ?? 0
+    // const { data: symbols } = useAllSymbols()
 
 
-//                     const bSymbolPrice = symbols?.find(s => s.name == b.symbol)?.final_price ?? 0
-//                     const bPSymbolPrice = symbols?.find(s => s.name == b.pSymbol)?.final_price ?? 0
+    return <Table >
+        <TableHead>
+            <TableRow className='text-center'>
+                <TableCell>نماد</TableCell>
+                <TableCell>نام اختیار</TableCell>
+                <TableCell>قیمت</TableCell>
+                {/* <TableCell>وضعیت</TableCell> */}
+                <TableCell>تاریخ قرارداد</TableCell>
+                <TableCell>سود اربیتراژ</TableCell>
+                <TableCell>سود درصد</TableCell>
+                {/* <TableCell>تاریخ پایان مهلت استفاده</TableCell>
+                <TableCell>مدت زمان باقی مانده تا پایان مرحله</TableCell>
+                <TableCell>برتر به سود بانکی 22%</TableCell>
+                <TableCell>برتر به بالاترین سود صندوق درامد ثابت 24%</TableCell> */}
+            </TableRow>
+        </TableHead>
+        <TableBody>
+            {data
+                // .sort((a, b) => {
+                //     const aSymbolPrice = symbols?.find(s => s.name == a.symbol)?.final_price ?? 0
+                //     const aPSymbolPrice = symbols?.find(s => s.name == a.pSymbol)?.final_price ?? 0
 
-//                     return ((Number(aSymbolPrice) / (Number(aPSymbolPrice) + 1000)) - 1) > ((Number(bSymbolPrice) / (Number(bPSymbolPrice) + 1000)) - 1) ? -1 : 1
-//                 })
-//                 .map(item => {
 
-//                     // const symbolPrice = symbols?.find(s => s.name == item.symbol)?.final_price ?? 0
-//                     // const pSymbolPrice = symbols?.find(s => s.name == item.pSymbol)?.final_price ?? 0
+                //     const bSymbolPrice = symbols?.find(s => s.name == b.symbol)?.final_price ?? 0
+                //     const bPSymbolPrice = symbols?.find(s => s.name == b.pSymbol)?.final_price ?? 0
 
-//                     // const symbol = symbols?.find(s => s.name == item.symbol)
+                //     return ((Number(aSymbolPrice) / (Number(aPSymbolPrice) + 1000)) - 1) > ((Number(bSymbolPrice) / (Number(bPSymbolPrice) + 1000)) - 1) ? -1 : 1
+                // })
+                .map(item => {
 
-//                     // const profitPercent = ((Number(symbolPrice) / (Number(pSymbolPrice) + 1000)) - 1) * 100
+                    // const symbolPrice = symbols?.find(s => s.name == item.symbol)?.final_price ?? 0
+                    // const pSymbolPrice = symbols?.find(s => s.name == item.pSymbol)?.final_price ?? 0
 
-//                     // const elepsedDays = Math.ceil(moment(item?.deadline, 'jYYYY/jMM/jDD').diff(moment(), 'day', true))
+                    // const symbol = symbols?.find(s => s.name == item.symbol)
 
-//                     // console.log(moment(item?.deadline ?? '', 'jYYYY/jMM/jDD'))
+                    // const profitPercent = ((Number(symbolPrice) / (Number(pSymbolPrice) + 1000)) - 1) * 100
 
-//                     return <TableRow className='text-center' hoverEffect>
-//                         <TableCell className='font-bold'>{symbol?.full_name}</TableCell>
-//                         <TableCell><a className='text-blue-800' href={`http://www.tsetmc.com/instInfo/${symbols?.find(s => s.name == item.symbol)?.instance_code}`} target='_blank'>{item.symbol}</a></TableCell>
-//                         <TableCell><a className='text-blue-800' href={`http://www.tsetmc.com/instInfo/${symbols?.find(s => s.name == item.pSymbol)?.instance_code}`} target='_blank'>{item.pSymbol}</a></TableCell>
-//                         {/* <TableCell>{item.state}</TableCell> */}
-//                         <TableCell>{Number(symbolPrice)?.toLocaleString()}</TableCell>
-//                         <TableCell>{Number(pSymbolPrice)?.toLocaleString()}</TableCell>
-//                         <TableCell className={profitPercent > 0 ? 'text-green-500 font-bold' : 'text-red-500 font-bold'}>{(symbolPrice && pSymbolPrice) ? Number(profitPercent).toFixed(2) : '-'}%</TableCell>
-//                         <TableCell>{item?.deadline} </TableCell>
-//                         <TableCell>{item?.deadline ? elepsedDays : '-'} روز</TableCell>
-//                         <TableCell className='align-middle text-center'>{(profitPercent / elepsedDays) > (22 / elepsedDays) ? <IconCheck color='green' /> : <IconX color='red' />}</TableCell>
-//                         <TableCell className='align-middle text-center'>{(profitPercent / elepsedDays) > (24 / elepsedDays) ? <IconCheck color='green' /> : <IconX color='red' />}</TableCell>
-//                         {/* <TableCell><a className='text-blue-800' href={`http://www.tsetmc.com/instInfo/${symbols?.find(s => s.name == item.symbol)?.instance_code}`} target='_blank'>لینک به TSE</a></TableCell> */}
-//                     </TableRow>
-//                 })}
-//         </TableBody>
-//     </Table>
+                    // const elepsedDays = Math.ceil(moment(item?.deadline, 'jYYYY/jMM/jDD').diff(moment(), 'day', true))
 
-// }
+                    // console.log(moment(item?.deadline ?? '', 'jYYYY/jMM/jDD'))
+
+                    return <TableRow className='text-center' hoverEffect>
+                        <TableCell className='font-bold'>{item?.full_name}</TableCell>
+                        <TableCell><a className='text-blue-800' href={`http://www.tsetmc.com/instInfo/${item.instance_code}`} target='_blank'>{item.name}</a></TableCell>
+                        {/* <TableCell>{item.state}</TableCell> */}
+                        <TableCell>{Number(item.final_price)?.toLocaleString()}</TableCell>
+                        {/* <TableCell>{Number(pSymbolPrice)?.toLocaleString()}</TableCell> */}
+                        {/* <TableCell className={profitPercent > 0 ? 'text-green-500 font-bold' : 'text-red-500 font-bold'}>{(symbolPrice && pSymbolPrice) ? Number(profitPercent).toFixed(2) : '-'}%</TableCell>
+                        <TableCell>{item?.deadline} </TableCell>
+                        <TableCell>{item?.deadline ? elepsedDays : '-'} روز</TableCell>
+                        <TableCell className='align-middle text-center'>{(profitPercent / elepsedDays) > (22 / elepsedDays) ? <IconCheck color='green' /> : <IconX color='red' />}</TableCell>
+                        <TableCell className='align-middle text-center'>{(profitPercent / elepsedDays) > (24 / elepsedDays) ? <IconCheck color='green' /> : <IconX color='red' />}</TableCell> */}
+                        {/* <TableCell><a className='text-blue-800' href={`http://www.tsetmc.com/instInfo/${symbols?.find(s => s.name == item.symbol)?.instance_code}`} target='_blank'>لینک به TSE</a></TableCell> */}
+                    </TableRow>
+                })}
+        </TableBody>
+    </Table>
+
+}
