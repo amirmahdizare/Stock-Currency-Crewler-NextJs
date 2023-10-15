a981aa85d46138bc3796ae3670bfc1016460543f
Amirmahdi <amirmahdikeshanzare@gmail.com>
2023-10-15 15:24:45 +0330

    Add Symbol Hook

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,6 +1,6 @@
 import { useQuery } from 'react-query'
 import { saGetRequest } from '../config/api'
-import { SACurrencyItemType, SASymbolListItemType } from '../types'
+import { FutureItemType, SACurrencyItemType, SASymbolListItemType } from '../types'
 
 export const useAllSymbols = () => useQuery<Array<SASymbolListItemType>>('getAllSymbols', () => saGetRequest<{ all: any, type: number }>()({ all: '', type: 2 }),
     {
@@ -9,7 +9,23 @@ export const useAllSymbols = () => useQuery<Array<SASymbolListItemType>>('getAll
         refetchInterval: false
     })
 
-export const useAllCurrencies = () => useQuery<{data :Array<SACurrencyItemType>}>('getAllCurrencies', () => saGetRequest<{ currency: string }>()({ currency: '' }),
+export const useAllCurrencies = () => useQuery<{ data: Array<SACurrencyItemType> }>('getAllCurrencies', () => saGetRequest<{ currency: string }>()({ currency: '' }),
+    {
+        refetchOnWindowFocus: false,
+        staleTime: 1000 * 60 * 10,
+        refetchInterval: false
+    })
+
+
+export const useFutureSymbols = () => useQuery<Array<FutureItemType>>('getFuture', async () => {
+    try {
+        
+        const res = await fetch('/api/ati')
+        return res?.json()
+    } catch (error) {
+        throw new Error('خطا در دریافت اطاعات لطفا دقایقی بعد تلاش کنید')
+    }
+},
     {
         refetchOnWindowFocus: false,
         staleTime: 1000 * 60 * 10,
